c680e3f43d6fade85e4790d7b96a3d4ecf9777b2
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-24 09:17:04 -0300

    Create new service for promotions

--- src/br/com/alura/rh/model/Funcionario.java
+++ src/br/com/alura/rh/model/Funcionario.java
@@ -23,6 +23,10 @@ public class Funcionario {
         this.dataUltimoReajuste = LocalDate.now();
     }
 
+    public void promover(Cargo novoCargo) {
+        this.cargo = novoCargo;
+    }
+
     public String getNome() {
         return nome;
     }
